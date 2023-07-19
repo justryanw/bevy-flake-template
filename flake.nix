eab881635329e5bddaff75dd02471e823ebddea0
Ryan Walker <ryanjwalker2001@gmail.com>
2023-07-19 22:33:54 +0100

    rust tool chain

--- flake.nix
+++ flake.nix
@@ -1,15 +1,21 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     naersk-src.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, flake-utils, naersk-src, nixpkgs }:
+  outputs = { self, flake-utils, rust-overlay, naersk-src, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        overlays = [ (import rust-overlay) 
+        (self: super: {
+          rustToolchain = super.rust-bin.stable.latest.default;
+        })];
+
         pkgs = (import nixpkgs) {
-          inherit system;
+          inherit system overlays;
         };
 
         naersk = pkgs.callPackage naersk-src { };
@@ -19,22 +25,23 @@
         ]);
 
         sharedBuildInputs = (with pkgs; [
-            libxkbcommon
-            libGL
-            alsa-lib
-            udev
+          libxkbcommon
+          libGL
+          alsa-lib
+          udev
 
-            # WINIT_UNIX_BACKEND=wayland
-            wayland
+          # WINIT_UNIX_BACKEND=wayland
+          wayland
 
-            # WINIT_UNIX_BACKEND=x11
-            xorg.libXcursor
-            xorg.libXrandr
-            xorg.libXi
-            xorg.libX11
+          # WINIT_UNIX_BACKEND=x11
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXi
+          xorg.libX11
         ]);
 
-      in with pkgs; {
+      in
+      with pkgs; {
         # For `nix build` & `nix run`:
         defaultPackage = naersk.buildPackage rec {
           src = ./.;
@@ -49,8 +56,7 @@
         # For `nix develop` (optional, can be skipped):
         devShell = pkgs.mkShell rec {
           nativeBuildInputs = sharedNativeBuildInputs ++ [
-            rustc
-            cargo
+            rustToolchain
           ];
 
           buildInputs = sharedBuildInputs;
