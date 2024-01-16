1e21d91f7a0b7e2485d3a3fda2733082380b5d5a
Ryan Walker <ryanjwalker2001@gmail.com>
2024-01-16 22:08:47 +0000

    deps

--- flake.nix
+++ flake.nix
@@ -9,16 +9,57 @@
   outputs = { self, nixpkgs, crane, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        pkgs = nixpkgs.legacyPackages.${system};
         craneLib = crane.lib.${system};
-      in
-    {
-      packages.default = craneLib.buildPackage {
-        src = craneLib.cleanCargoSource (craneLib.path ./.);
 
-        # Add extra inputs here or any other derivation settings
-        # doCheck = true;
-        # buildInputs = [];
-        # nativeBuildInputs = [];
-      };
-    });
+        buildDeps = (with pkgs; [
+          pkg-config
+          makeWrapper
+          clang
+          mold
+        ]);
+
+        runtimeDeps = (with pkgs; [
+          libxkbcommon
+          alsa-lib
+          udev
+          vulkan-loader
+          wayland
+        ] ++ (with xorg; [
+          libXcursor
+          libXrandr
+          libXi
+          libX11
+        ]));
+
+        sharedAttrs = rec {
+          pname = "bevy-flake-template";
+          src = ./.;
+
+          nativeBuildInputs = buildDeps;
+          buildInputs = runtimeDeps;
+
+          overrideMain = attrs: {
+            fixupPhase = ''
+              wrapProgram $out/bin/${pname} \
+                --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath runtimeDeps} \
+                --prefix XCURSOR_THEME : "Adwaita"
+              mkdir -p $out/bin/assets
+              cp -a assets $out/bin
+            '';
+          };
+        };
+      in
+      {
+        packages.default = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+          nativeBuildInputs = buildDeps;
+          buildInputs = runtimeDeps;
+          # Add extra inputs here or any other derivation settings
+          # doCheck = true;
+          # buildInputs = [];
+          # nativeBuildInputs = [];
+        };
+      });
 }
