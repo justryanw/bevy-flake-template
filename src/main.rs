d2195a2341f40f002fe9eb0860f612fa908481bc
Ryan Walker <ryanjwalker2001@gmail.com>
2024-01-18 10:01:19 +0000

    bevy app

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,27 @@
-// use bevy::prelude::*;
-
-// fn main() {
-//     App::new()
-//         .insert_resource(ClearColor(Color::BLACK))
-//         .add_plugins(DefaultPlugins)
-//         .add_systems(Startup, setup)
-//         .add_systems(Update, rotate)
-//         .run();
-// }
-
-// fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-//     // Camera
-//     commands.spawn(Camera2dBundle::default());
-
-//     // Sprite
-//     commands.spawn(SpriteBundle {
-//         texture: asset_server.load("bevy.png"),
-//         ..default()
-//     });
-// }
-
-// fn rotate(mut query: Query<&mut Transform, With<Sprite>>, time: Res<Time>) {
-//     for mut bevy in &mut query {
-//         bevy.rotate_local_z(-time.delta_seconds());
-//     }
-// }
+use bevy::prelude::*;
 
 fn main() {
-    println!("Hello World");
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, rotate)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+
+    // Sprite
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("bevy.png"),
+        ..default()
+    });
+}
+
+fn rotate(mut query: Query<&mut Transform, With<Sprite>>, time: Res<Time>) {
+    for mut bevy in &mut query {
+        bevy.rotate_local_z(-time.delta_seconds());
+    }
 }
