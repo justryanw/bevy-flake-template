cc75cfbc94d2dd3bed8fdeeffb051e567f922273
Ryan Walker <ryanjwalker2001@gmail.com>
2024-06-09 20:46:27 +0100

    bevy

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,53 @@
-use yew::prelude::*;
-
-#[function_component]
-fn App() -> Html {
-    html! {
-      <h1>
-        {"Hello world!"}
-      </h1>
-    }
-}
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy game".to_string(), // ToDo
+                // Bind to canvas included in `index.html`
+                canvas: Some("#bevy".to_owned()),
+                // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                prevent_default_event_handling: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .add_systems(Update, rotate)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    // asset_server: Res<AssetServer>
+) {
+    // Camera
+    commands.spawn(Camera2dBundle::default());
+
+    // Sprite
+    // commands.spawn(SpriteBundle {
+    //     texture: asset_server.load("bevy.png"),
+    //     ..default()
+    // });
+
+    // Cube
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Rectangle::default()).into(),
+        transform: Transform::default().with_scale(Vec3::splat(128.)),
+        material: materials.add(Color::PURPLE),
+        ..default()
+    });
+}
+
+fn rotate(mut query: Query<&mut Transform, With<Mesh2dHandle>>, time: Res<Time>) {
+    for mut bevy in &mut query {
+        bevy.rotate_local_z(-time.delta_seconds());
+    }
 }
