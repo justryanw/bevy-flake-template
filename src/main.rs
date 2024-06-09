4d25bf5619882d7efd8412e5acc8220a441b783b
Ryan Walker <ryanjwalker2001@gmail.com>
2024-06-10 00:59:20 +0100

    assets

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use bevy::{
-    prelude::*,
-    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
-};
+use bevy::{asset::AssetMetaCheck, prelude::*};
 
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
+        .insert_resource(AssetMetaCheck::Never)
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Bevy game".to_string(), // ToDo
@@ -24,29 +22,19 @@ fn main() {
 
 fn setup(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    // asset_server: Res<AssetServer>
+    asset_server: Res<AssetServer>,
 ) {
     // Camera
     commands.spawn(Camera2dBundle::default());
 
     // Sprite
-    // commands.spawn(SpriteBundle {
-    //     texture: asset_server.load("bevy.png"),
-    //     ..default()
-    // });
-
-    // Cube
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(Rectangle::default()).into(),
-        transform: Transform::default().with_scale(Vec3::splat(128.)),
-        material: materials.add(Color::PURPLE),
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("bevy.png"),
         ..default()
     });
 }
 
-fn rotate(mut query: Query<&mut Transform, With<Mesh2dHandle>>, time: Res<Time>) {
+fn rotate(mut query: Query<&mut Transform, With<Sprite>>, time: Res<Time>) {
     for mut bevy in &mut query {
         bevy.rotate_local_z(-time.delta_seconds());
     }
